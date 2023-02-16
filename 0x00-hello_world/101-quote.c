c39b233283e98fa9f6838535d6b138cb1d087272
Oluwatosin Adegoke <amosadex2010@gmail.com>
2023-02-16 15:28:47 -0800

    Task 8 is done and completed

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - a program to print a string
+ *
+ *
+ * Return: the program will retun an error
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
