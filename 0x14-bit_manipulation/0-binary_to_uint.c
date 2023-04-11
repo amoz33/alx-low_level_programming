043a5d340ca9dd0d3a9d2a393bc9e65b5e433cdc
Oluwatosin Adegoke <amosadex2010@gmail.com>
2023-04-11 22:30:48 +0100

    Updated task 0 file from bugs

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,30 +1,33 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * binary_to_uint - A finction that converts binary number
- * to an unsigned integer
- * @b: A pointer to binary string
- * Return: unsigned integer converted number
+ * binary_to_uint - func that convert a binary number to an unsigned int
+ *
+ * @b: char string
+ *
+ * Return: converted decimal number or 0 if there is an unconvertable char
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int base = 1, result = 0, len = 0;
+	unsigned int total, tower;
+	int len;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[len])
-		len++;
-
-	while (len)
+	for (len = 0; b[len]; len++)
 	{
-		if (b[len - 1] != '0' && b[len - 1] != '1')
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
-
-		if (b[len - 1] == '1')
-			result += base;
-		base *= 2;
-		len--;
 	}
-	return (result);
+
+	for (tower = 1, total = 0, len--; len >= 0; len--, tower *= 2)
+	{
+		if (b[len] == '1')
+			total += tower;
+	}
+
+	return (total);
 }
